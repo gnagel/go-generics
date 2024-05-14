1aa1e9b1a485a471ee08856bacc3988f9893886b
Glenn Nagel <glenn@mercury-wireless.com>
2024-05-14 01:33:10 -0400

    Add tests + benchmarks for interning

--- intern/intern_test.go
+++ intern/intern_test.go
@@ -31,20 +31,19 @@ func TestIntern(t *testing.T) {
 	i.Clear()
 	require.Zero(t, i.Len())
 
-	inputs := []string{faker.Word(), faker.Word(), faker.Word()}
-	inputs = append(inputs, inputs...)
-	inputs = append(inputs, inputs...)
-	rand.Shuffle(len(inputs), func(i, j int) {
-		inputs[i] = inputs[j]
-	})
+	inputs := randomStringInputs(1_00)
+	unique := make(map[string]string, len(inputs))
+	for _, k := range inputs {
+		unique[k] = k
+	}
 
 	keys := map[uint64]uint64{}
 	for _, input := range inputs {
 		key := i.Insert(input)
 		keys[key] = key
 	}
-	require.Len(t, keys, 3)
-	require.EqualValues(t, i.Len(), 3)
+	require.Len(t, keys, len(unique))
+	require.EqualValues(t, i.Len(), len(unique))
 }
 
 func TestSafeIntern(t *testing.T) {
@@ -68,12 +67,11 @@ func TestSafeIntern(t *testing.T) {
 	i.Clear()
 	require.Zero(t, i.Len())
 
-	inputs := []string{faker.Word(), faker.Word(), faker.Word()}
-	inputs = append(inputs, inputs...)
-	inputs = append(inputs, inputs...)
-	rand.Shuffle(len(inputs), func(i, j int) {
-		inputs[i] = inputs[j]
-	})
+	inputs := randomStringInputs(1_00)
+	unique := make(map[string]string, len(inputs))
+	for _, k := range inputs {
+		unique[k] = k
+	}
 
 	var wg sync.WaitGroup
 	for idx := 0; idx < 10; idx++ {
@@ -86,5 +84,51 @@ func TestSafeIntern(t *testing.T) {
 		}()
 	}
 	wg.Wait()
-	require.EqualValues(t, i.Len(), 3)
+	require.EqualValues(t, i.Len(), len(unique))
+}
+
+func BenchmarkIntern(b *testing.B) {
+	inputs := randomStringInputs(10_000)
+
+	i := New[string]()
+
+	for index := 0; index < b.N; index++ {
+		i.Deduplicate(inputs[index%len(inputs)])
+	}
+}
+
+func BenchmarkSafeIntern(b *testing.B) {
+	inputs := randomStringInputs(10_000)
+
+	i := NewSafe[string]()
+
+	rows := make(chan string)
+
+	var wg sync.WaitGroup
+	for idx := 0; idx < 10; idx++ {
+		wg.Add(1)
+		go func() {
+			defer wg.Done()
+			for row := range rows {
+				i.Deduplicate(row)
+			}
+		}()
+	}
+
+	for index := 0; index < b.N; index++ {
+		rows <- inputs[index%len(inputs)]
+	}
+	close(rows)
+	wg.Wait()
+}
+
+func randomStringInputs(maxLen int) []string {
+	inputs := make([]string, maxLen)
+	for index := 0; index < len(inputs); index++ {
+		inputs[index] = faker.Sentence()
+	}
+	rand.Shuffle(len(inputs), func(i, j int) {
+		inputs[i] = inputs[j]
+	})
+	return inputs
 }
